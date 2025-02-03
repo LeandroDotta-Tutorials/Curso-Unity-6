05104b25da12f9f8981ed4c868cefdb334c43efe
Leandro Dotta <leandrodotta@gmail.com>
2025-02-02 22:46:42 -0300

    add an option to move the object forward (up) with the AutoMovement component

--- projects/Cyber Jungle Infinity/Assets/Scripts/AutoMovement.cs
+++ projects/Cyber Jungle Infinity/Assets/Scripts/AutoMovement.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class AutoMovement : MonoBehaviour
 {
     public float speed = 5;
+    public bool moveFront;
     public Vector2 direction = Vector2.down;
 
     private void Update()
     {
-        transform.Translate(speed * Time.deltaTime * direction);
+        Vector2 dir = moveFront ? transform.up : direction;
+        transform.Translate(speed * Time.deltaTime * dir, Space.World);
     }
 }
