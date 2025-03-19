cca29434e53481fd0d4ac9779d84680369801963
Leandro Dotta <leandrodotta@gmail.com>
2025-03-18 22:50:10 -0300

    remove unused Debug log message

--- projects/Cyber Jungle Infinity/Assets/Scripts/DestroyWhenNotVisible.cs
+++ projects/Cyber Jungle Infinity/Assets/Scripts/DestroyWhenNotVisible.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class DestroyWhenNotVisible : MonoBehaviour
 {
     private void OnBecameInvisible()
-    {        
-        Debug.Log($"DestroyWhenNotVisible: {gameObject.name}");
+    {
         Destroy(gameObject);
     }
 }
