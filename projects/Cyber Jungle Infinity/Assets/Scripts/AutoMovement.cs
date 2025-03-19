32cbb9de5b14b3cf81896d966172e2a665b55faf
Leandro Dotta <leandrodotta@gmail.com>
2025-03-18 22:48:45 -0300

    remove move front option from AutoMovement script

--- projects/Cyber Jungle Infinity/Assets/Scripts/AutoMovement.cs
+++ projects/Cyber Jungle Infinity/Assets/Scripts/AutoMovement.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 public class AutoMovement : MonoBehaviour
 {
     public float speed = 5;
-    public bool moveFront;
     public Vector2 direction = Vector2.down;
 
     private void Update()
     {
-        Vector2 dir = moveFront ? transform.up : direction;
-        transform.Translate(speed * Time.deltaTime * dir, Space.World);
+        transform.Translate(speed * Time.deltaTime * direction, Space.World);
     }
 }
